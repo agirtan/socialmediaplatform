4ccdcf2736aaf52db8475ee2453060de8a38d85f
Alexandru Girtan <123776393+agirtan@users.noreply.github.com>
2024-01-28 21:27:21 +0100

    Add files via upload
    Java Security

---
+++ demo/src/main/java/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.login.login.app2.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class AuthenticationRequest {
+
+    private String email;
+    private String password;
+}
